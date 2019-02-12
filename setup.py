e24c3a3446dadadf8b0793593d5a7fa6cbb7cb4b
s-weigand <s.weigand.phy@gmail.com>
2019-02-12 18:24:44 +0100

    first playing around with dash for plotting and it looks quite nice

--- setup.py
+++ setup.py
@@ -15,7 +15,8 @@ requirements = ['Click>=6.0',
                 'pandas>=0.23.4',
                 'PyQt5>=5.6.0',
                 'pysftp>=0.2.9',
-                'holidays>=0.9.8']
+                'holidays>=0.9.8',
+                'dash>=0.37.0']
 
 setup_requirements = [ ]
 
---
+++ work_tracker/plotting/dashboard.py
@@ -0,0 +1,140 @@
+import sys
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objs as go
+import traceback
+
+import flask
+import pandas as pd
+import time
+import os
+
+def timedelt_to_float_h(timedeltas):
+    seconds = timedeltas.total_seconds()
+    return seconds/3600
+
+server = flask.Flask('app')
+server.secret_key = os.environ.get('secret_key', 'secret')
+
+
+stechkarte = pd.read_csv('../../mydata/.stechkarte_local.csv',
+                         sep="\t", parse_dates=["start", "end"])
+stechkarte["worktime"] = stechkarte["end"] - stechkarte["start"]
+
+min_date = stechkarte["start"].min()
+max_date = stechkarte["start"].max() + pd.Timedelta(days=31)
+
+occupations = stechkarte["occupation"].unique()
+option_list = []
+for occupation in occupations:
+    option_list.append({"label": occupation, "value": occupation})
+
+app = dash.Dash('app', server=server)
+
+app.scripts.config.serve_locally = False
+dcc._js_dist[0]['external_url'] = 'https://cdn.plot.ly/plotly-basic-latest.min.js'
+
+app.layout = html.Div([
+    html.H1('Worktracker plot'),
+    dcc.Dropdown(
+        id='my-dropdown',
+        options=option_list,
+        value=occupations,
+        multi=True,
+    ),
+    dcc.DatePickerSingle(
+        id="min-date",
+        date=min_date,
+        display_format='YYYY-MM-DD'
+    ),
+    dcc.DatePickerSingle(
+        id="max-date",
+        date=max_date,
+        display_format='YYYY-MM-DD'
+    ),
+    dcc.Graph(id='my-graph')
+], className="container")
+
+
+@app.callback(Output('my-graph', 'figure'),
+              [Input('my-dropdown', 'value'), Input('min-date', 'date'), Input('max-date', 'date')])
+
+def update_graph(selected_dropdown_value, sel_min_date, sel_max_date):
+    # dff = df[df['Stock'] == selected_dropdown_value]
+    # plotdata = stechkarte[stechkarte["occupation"] == selected_dropdown_value]
+    # worktime = plotdata.resample('M', on='start').sum()["worktime"].apply(timedelt_to_float_h).reset_index()
+    plot_list = []
+    # if selected_dropdown_value == "Total":
+    #     for occupation in occupations:
+    #         plotdata = stechkarte[stechkarte["occupation"] == occupation]
+    #         worktime = plotdata.resample('M', on='start').sum()["worktime"].apply(timedelt_to_float_h).reset_index()
+    #         plot_list.append({
+    #             'x': worktime.start,
+    #             'y': worktime.worktime,
+    #             'name': occupation,
+    #             'type': 'bar'
+    #         })
+    # else:
+    #     plotdata = stechkarte[stechkarte["occupation"] == selected_dropdown_value]
+    #     worktime = plotdata.resample('M', on='start').sum()["worktime"].apply(timedelt_to_float_h).reset_index()
+    #     plot_list.append({
+    #         'x': worktime.start,
+    #         'y': worktime.worktime,
+    #         'name': selected_dropdown_value,
+    #         'type': 'bar'
+    #     })
+    print("sel_min_date", sel_min_date, file=sys.stderr)
+    print("type(sel_min_date)", type(sel_min_date), file=sys.stderr)
+    sel_min_date = pd.to_datetime(sel_min_date)
+    sel_max_date = pd.to_datetime(sel_max_date)
+    try:
+        ranged_stechkarte = stechkarte[((stechkarte["start"]>= sel_min_date) & (stechkarte["start"]<= sel_max_date))]
+        for occupation in selected_dropdown_value:
+            plotdata = ranged_stechkarte[ranged_stechkarte["occupation"] == occupation]
+            worktime = plotdata.resample('M', on='start').sum()["worktime"].apply(timedelt_to_float_h)
+            if not worktime.empty:
+                plot_list.append({
+                    'x': worktime.index,
+                    'y': worktime.values,
+                    'name': occupation,
+                    'type': 'bar'
+                })
+        # Total
+        worktime = ranged_stechkarte.resample('M', on='start').sum()["worktime"].apply(timedelt_to_float_h)
+
+        plot_list.append(go.Scatter(
+            x= worktime.index,
+            y= worktime.values,
+            name= "Total",
+            mode= 'markers',
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+        ))
+
+        plot_list.append({
+            'x': [sel_min_date, sel_max_date],
+            'y': [80, 80],
+            'name': "contract time",
+            'type': 'line'
+        })
+    except Exception as e:
+        traceback.print_exc()
+        print(ranged_stechkarte.head(), file=sys.stderr)
+        print(worktime.head(), file=sys.stderr)
+
+    return {
+        'data': plot_list,
+        'layout': {
+            'barmode':'stack',
+            "xaxis":{'title': 'Date'},
+            "yaxis":{'title': 'Work Time'},
+            "margin":{'l': 40, 'b': 40, 't': 10, 'r': 10},
+        }
+    }
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
